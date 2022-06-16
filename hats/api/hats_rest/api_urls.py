49c7673ba61a9c6c53852750a3bc30bb4131bbbb
Junella Caringal <junellacaringal@gmail.com>
2022-06-16 14:22:21 -0700

    Changed url path for hats list

--- hats/api/hats_rest/api_urls.py
+++ hats/api/hats_rest/api_urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .api_views import api_list_hats, api_detail_hat
 
 urlpatterns = [
-    path("locations/<int:location_vo_id>/hats/", api_list_hats, name="api_list_hats"),
+    path("hats/", api_list_hats, name="api_list_hats"),
     path("hats/", api_list_hats, name="api_create_hat"),
-    path("hats/<int:pk>/", api_detail_hat, name="api_detail_hat")
+    path("hats/<int:pk>/", api_detail_hat, name="api_detail_hat"),
 ]
