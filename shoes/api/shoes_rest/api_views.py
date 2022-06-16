7a4507e1a6e1473ac4e6793ef2a903bf08de30a1
wilson <wilsnyeh@gmail.com>
2022-06-16 04:42:43 -0700

    updated views

--- shoes/api/shoes_rest/api_views.py
+++ shoes/api/shoes_rest/api_views.py
@@ -1,16 +1,63 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from json
+import json
 
 from common.json import ModelEncoder
 from .models import BinVO, Shoes
 
 class BinVODetailEncoder(ModelEncoder):
     model = BinVO
-    properties = ['name']
+    properties = ['closet_name','import_href']
 
 class ShoesListEncoder(ModelEncoder):
     model = Shoes
-    properties = ['name']
+    properties = ['model_name']
 
-class ShoeDetailEncoder(ModelEncoder)
+    def get_extra_data(self, o):
+        return {'bin': o.bin.closet_name}
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoes
+    properties = [
+        "model_name",
+        "manufacturer",
+        "color",
+        "bin",
+    ]
+    encoders = {
+        "bin": BinVODetailEncoder(),
+    }
+
+    #do i need get extra data here for something? 
+
+@require_http_methods(["GET", "POST"])
+def api_shoes(request, bin_vo_id=None):
+    if request.method == 'GET':
+        if bin_vo_id is not None:
+            shoes = Shoes.objects.filter(bin=bin_vo_id)
+        else:
+            shoes = Shoes.objects.all()
+            return JsonResponse(
+                {'shoes': shoes},
+                encoder=ShoesListEncoder,
+            )
+    else: 
+        content = json.loads(request.body)
+        try:
+            bin_href = content['bin']
+            bin = BinVO.objects.get(import_href=bin_href)
+            content['bin'] = bin
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {'message': 'Invalid bin id'},
+                status=400,
+            )
+
+
+def api_show_shoe(request, pk):
+    shoe = Shoes.objects.get(id=pk)
+    return JsonResponse(
+        shoe,
+        encoder=ShoeDetailEncoder,
+        safe=False,
+    )
--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -5,11 +5,12 @@ class BinVO(models.Model):
     closet_name = models.CharField(max_length=100)
     bin_number = models.PositiveSmallIntegerField()
     bin_size = models.PositiveSmallIntegerField()
+    import_href = models.CharField(max_length=200, unique=True)
 class Shoes(models.Model):
     manufacturer = models.CharField(max_length=200)
     model_name = models.CharField(max_length=200)
     color = models.CharField(max_length=100)
-    pictureUrl = models.URLField(max_length=200)
+    pictureUrl = models.URLField(null=True)
     bin = models.ForeignKey(BinVO, related_name='shoes', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
