cf73c18fd02a1391266cc05e088e1aaff204860c
Junella Caringal <junellacaringal@gmail.com>
2022-06-15 22:42:12 -0700

    Added url paths for hats list and create

--- hats/api/hats_rest/api_urls.py
+++ hats/api/hats_rest/api_urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from .api_views import api_list_hats
 
 urlpatterns = [
-    path("locations/<int:location_vo_id>/hats/", api_list_hats, name="api_list_hats")
+    path("locations/<int:location_vo_id>/hats/", api_list_hats, name="api_list_hats"),
+    path("hats/", api_list_hats, name="api_create_hat"),
 ]
